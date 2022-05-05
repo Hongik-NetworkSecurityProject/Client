c94973ddc84e4c67dfc66a126fe8e21c0f7e7d94
yongCloude <djkye0001@naver.com>
2022-05-05 17:40:38 +0900

    [doc] macro 많이 추가

---
+++ src/include/macro.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+
+#define TRUE 1
+#define FALSE 0
+
+
+#define ID_SIZE 16
+#define CHALLENGE_TS_SIZE 16
+#define SYM_KEY_SIZE 32
+#define HASH_SIZE 32
+#define AUTH_MSG_SIZE 64
+#define TOKEN_SIZE 64
+#define MSG_SIZE 128
+#define RSA_ENC_SIZE 256
+#define SYM_KEY_BIT 256
+
+#define BUF_SIZE 1024
+#define PUBLIC_KEY_BITS 2056
+
+#define FILE_SIZE 1000000
+
+#define AUTH 1
+#define FILE 2
